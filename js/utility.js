d3641a2d7c3ba8fc70893f408e5f90af8d2e62cd
programing360 <fhlimon360@gmail.com>
2025-02-07 22:23:18 +0600

    added finish donation project

--- js/utility.js
+++ js/utility.js
@@ -11,7 +11,7 @@
 // }
 document.getElementById('donation-form')
     .addEventListener('scroll', function () {
-        document.getElementById('navbar').classList.add(' backdrop-blur-md')
+        document.getElementById('navbar').classList.add('backdrop-blur-md')
     })
 document.getElementById('donate-now-btn')
     .addEventListener('click', function () {
@@ -38,31 +38,162 @@ document.getElementById('donate-now-btn')
             const getCurrentDonationAmount = document.getElementById('get-current-amount').innerText;
             const getCurrentDonationAmountP = parseFloat(getCurrentDonationAmount)
             const B = getCurrentDonationAmountP - inputValueAmountP;
-            
-            const historyDetails =  document.getElementById('history-form')
-           historyDetails.append(div);
+
             if (B <= 0) {
                 alert('Do not enough Balance! Please earn Money.')
             }
 
             document.getElementById('get-current-amount').innerText = B
+
+
+
             // history set----------------------------------------------
-            const div = document.createElement('div')
+            const div = document.createElement('p')
             div.innerHTML = `
-            <h1 class = font-bold text-xl mb-4>${newBalance} Taka is Donated for Flood at Noakhali, Bangladesh </h1>
+            <h1 class = font-bold text-xl mb-4>${inputValueAmountP} Taka is Donated for Flood at Noakhali, Bangladesh </h1>
             <p>Date:${new Date}</p>
             `
             console.log(div)
-           
+
+            const historyDetails = document.getElementById('history-form')
+            historyDetails.append(div);
         }
         else {
             alert('Validate the donation amount!')
         }
 
+        document.getElementById('history-btn')
+            .addEventListener('click', function () {
+                document.getElementById('history-form').classList.remove('hidden')
+
+            })
         document.getElementById('close-config')
             .addEventListener('click', function () {
                 document.getElementById('Congrates-form').classList.add('hidden')
-                
+
             })
 
     })
+
+// Donate Now btn 2------------------------------------------------------------------------------------
+document.getElementById('donate-now-btn-2')
+    .addEventListener('click', function () {
+        document.getElementById('Congrates-form').classList.remove('hidden')
+    const inputValueAmount = document.getElementById('input-get-amount-2').value;
+    const inputValueAmountP = parseFloat(inputValueAmount)
+    // console.log(typeof inputValueAmountP)
+
+    if (inputValueAmountP < 0) {
+        alert('Validate the donation amount')
+    }
+    if (inputValueAmountP === inputValueAmountP) {
+
+        const getDonationAmount = document.getElementById('get-total-bdt-2').innerText
+        const getDonationAmountP = parseFloat(getDonationAmount)
+        // console.log(typeof getDonationAmountP)
+
+        // console.log(typeof inputValueAmountP)
+        const s = inputValueAmountP + getDonationAmountP;
+        const newBalance = parseFloat(s)
+        document.getElementById('get-total-bdt-2').innerText = newBalance;
+
+        const getCurrentDonationAmount = document.getElementById('get-current-amount').innerText;
+        const getCurrentDonationAmountP = parseFloat(getCurrentDonationAmount)
+        const B = getCurrentDonationAmountP - inputValueAmountP;
+
+        if (B <= 0) {
+            alert('Do not enough Balance! Please earn Money.')
+        }
+
+        document.getElementById('get-current-amount').innerText = B
+
+
+
+        // history set----------------------------------------------
+        const div = document.createElement('p')
+        div.innerHTML = `
+            <h1 class = font-bold text-xl mb-4>${inputValueAmountP} Aid for Injured in the Quota Movement</h1>
+            <p>Date:${new Date}</p>
+            `
+        console.log(div)
+
+        const historyDetails = document.getElementById('history-form')
+        historyDetails.append(div);
+    }
+    else {
+        alert('Validate the donation amount!')
+    }
+
+    document.getElementById('history-btn')
+        .addEventListener('click', function () {
+            document.getElementById('history-form').classList.remove('hidden')
+
+        })
+    document.getElementById('close-config')
+        .addEventListener('click', function () {
+            document.getElementById('Congrates-form').classList.add('hidden')
+
+        })
+    })
+
+    // Donate Now btn 3------------------------------------------------------------------------------------
+    document.getElementById('donate-now-btn-3')
+    .addEventListener('click', function () {
+        document.getElementById('Congrates-form').classList.remove('hidden')
+    const inputValueAmount = document.getElementById('input-get-amount-3').value;
+    const inputValueAmountP = parseFloat(inputValueAmount)
+    // console.log(typeof inputValueAmountP)
+
+    if (inputValueAmountP < 0) {
+        alert('Validate the donation amount')
+    }
+    if (inputValueAmountP === inputValueAmountP) {
+
+        const getDonationAmount = document.getElementById('get-total-bdt-3').innerText
+        const getDonationAmountP = parseFloat(getDonationAmount)
+        // console.log(typeof getDonationAmountP)
+
+        // console.log(typeof inputValueAmountP)
+        const s = inputValueAmountP + getDonationAmountP;
+        const newBalance = parseFloat(s)
+        document.getElementById('get-total-bdt-3').innerText = newBalance;
+
+        const getCurrentDonationAmount = document.getElementById('get-current-amount').innerText;
+        const getCurrentDonationAmountP = parseFloat(getCurrentDonationAmount)
+        const B = getCurrentDonationAmountP - inputValueAmountP;
+
+        if (B <= 0) {
+            alert('Do not enough Balance! Please earn Money.')
+        }
+
+        document.getElementById('get-current-amount').innerText = B
+
+
+
+        // history set----------------------------------------------
+        const div = document.createElement('p')
+        div.innerHTML = `
+            <h1 class = font-bold text-xl mb-4>${inputValueAmountP} Donate for Flood Relief in Feni,Bangladesh</h1>
+            <p>Date:${new Date}</p>
+            `
+        console.log(div)
+
+        const historyDetails = document.getElementById('history-form')
+        historyDetails.append(div);
+    }
+    else {
+        alert('Validate the donation amount!')
+    }
+
+    document.getElementById('history-btn')
+        .addEventListener('click', function () {
+            document.getElementById('history-form').classList.remove('hidden')
+
+        })
+    document.getElementById('close-config')
+        .addEventListener('click', function () {
+            document.getElementById('Congrates-form').classList.add('hidden')
+
+        })
+    })
+
